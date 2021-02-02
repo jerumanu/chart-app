75f0ab2004433080bab74c7d821fd43794c93fbe
jerumanu <emmanueljeru200@email.com>
2021-02-02 19:01:27 +0300

    changes made

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,22 +9,23 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        
+
         await self.accept()
 
-      
-
-    async def disconnect(self,close_code):
-       await self.channel_layer.group_discard(
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
-        )  
+        )
 
-    async def receive (self,text_data):
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
@@ -37,14 +38,12 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             }
         )
 
-
-    async def chatroom_message(self,event):
+    # Receive message from room group
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
-
-      
         
